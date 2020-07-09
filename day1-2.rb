f6acc87248e395b6a7c40e19d71dc9f1cfe3d590
Ted Chen <teddy70642000@gmail.com>
2020-07-09 18:41:13 +0800

    0709 ruby

---
+++ day1-2.rb
@@ -0,0 +1,23 @@
+# def a
+#     "aaa"
+# end
+
+# def b 
+#     puts "bbb"
+# end
+
+# a && b
+def a 
+    return 1
+end
+
+def b  
+    return 2
+end
+
+result1 = a && b   
+puts result1
+
+
+result2 = a || b   
+puts result2
